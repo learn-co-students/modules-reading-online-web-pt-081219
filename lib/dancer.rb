02f85f14c7bbb7554fe4580fcd1205a1be45aecb
LUIS J Alvarez Diaz <ldiaz244@gmail.com>
2019-09-27 02:36:47 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,10 @@
-require_relative './class_methods_module.rb'
-require_relative './dance_module.rb'
+class Dancer
+include Dance
+
+attr_accessor :name
+
+def initializ(name)
+  @name = name
+class Dancer
+  extend MetaDancing
+end
